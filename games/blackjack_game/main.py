9ea3affb4ef6846155893aa8bb7b108bd3362150
Igor Donchovski <doncovski_igor@yahoo.com>
2024-02-27 16:49:08 +0100

    Initial commit

--- games/blackjack_game/main.py
+++ games/blackjack_game/main.py
@@ -1,5 +1,3 @@
-############### Blackjack Project #####################
-
 import random
 from art import logo
 
@@ -77,6 +75,5 @@ def play_game():
     print(compare(user_score, computer_score))
 
 
-# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
     play_game()
