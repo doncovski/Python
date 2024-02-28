e42b2384eca4173f8647d4baeac7c17d31b05350
Igor Donchovski <doncovski_igor@yahoo.com>
2024-02-28 13:54:54 +0100

    Initial commit

---
+++ password_generator/main.py
@@ -0,0 +1,14 @@
+from random import randint, choice, shuffle
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+password_list = [choice(letters) for char in range(randint(8, 10))] + [choice(symbols) for sym in
+                                                                       range(randint(2, 4))] + [choice(numbers) for num
+                                                                                                in range(randint(2, 4))]
+shuffle(password_list)
+password = "".join(password_list)
+print(f"Your password is: {password}")
